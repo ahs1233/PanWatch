83cfb215527ff44db18c04c17f4d824a5c40873b
ahs1233 <ah.hero95@gmail.com>
2026-09-22 14:32:38 +0300

    test(benchmark): 更新 v1.3 coverage 断言

--- ci_tests/test_panwatch_benchmark_v1.py
+++ ci_tests/test_panwatch_benchmark_v1.py
@@ -20,8 +20,8 @@ def test_panwatch_benchmark_v1_is_explicit_about_unautomated_tracks():
     report = run_benchmark()
     coverage = report["track_coverage"]
     assert coverage == {
-        "automated": 3,
+        "automated": 4,
         "specified_not_automated": 1,
-        "total": 4,
+        "total": 5,
     }
     assert any("not automated" in item for item in report["limitations"])
